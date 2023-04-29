1cae16e74fdd5d33df6709cfc9aa2377df57d21f
Neel Bhalla <neelbhallabos@gmail.com>
2023-04-29 18:04:30 -0400

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,19 @@
+from flask import Flask, request
+from subprocess import Popen
+from socket import gethostname, gethostbyname
+
+app = Flask(__name__)
+
+seed = 0
+@app.route('/', methods = ['GET'])
+def get_seed():
+    hn = gethostname()
+    return gethostbyname(hn), 200
+
+@app.route('/', methods = ['POST'])
+def update_seed():
+    process = Popen(['python3', 'stress_cpu.py'])
+    return "STARTED", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
